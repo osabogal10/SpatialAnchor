1357e348ea3cf8e172201a3e67b6b80522800aeb
osabogal10 <o.sabogal10@uniandes.edu.co>
2019-02-10 22:32:34 -0500

    SpatialAnchor init

---
+++ Assets/AnchorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+public class AnchorScript : MonoBehaviour
+{
+    public WorldAnchorManager worldAnchorManager;
+
+    public void AnchorIt()
+    {
+        worldAnchorManager.AttachAnchor(this.gameObject);
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void ReleaseAnchor()
+    {
+        worldAnchorManager.RemoveAnchor(this.gameObject);
+        this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnchorIt();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
